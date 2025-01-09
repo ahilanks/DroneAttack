b68bd4aa7a40acb2ff6b175bb5bb3fdde8632f8c
Ahilan <ahilanks101@gmail.com>
2025-01-08 21:59:21 -0800

    Initial Commit

---
+++ DJIDroneTests/DJIDroneTests.swift
@@ -0,0 +1,17 @@
+//
+//  DJIDroneTests.swift
+//  DJIDroneTests
+//
+//  Created by Ahilan Karuppusami on 1/8/25.
+//
+
+import Testing
+@testable import DJIDrone
+
+struct DJIDroneTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
